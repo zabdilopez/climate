3bebb22507ebdb1f3b4b99e84e7149954092873a
Zabdi Lopez <zabdi@alumni.uvg.edu.gt>
2023-07-11 21:26:53 +0200

    cumulative precipitation final

---
+++ scripts/cumulative_prcp.R
@@ -0,0 +1,46 @@
+library(ggtext)
+library(scales)
+
+source("scripts/local_weather.R")
+
+this_year <- year(today())
+this_month <- month(today(), label = TRUE, abbr = FALSE)
+this_day <- ordinal(day(today()))
+
+local_weather %>% 
+  select(date, prcp) %>% 
+  drop_na(prcp) %>% 
+  mutate(year = year(date),
+         month = month(date),
+         day = day(date),
+         is_this_year = year == this_year) %>% 
+  filter(year !=1981 & !(month == 2 & day == 29)) %>% 
+  group_by(year) %>% 
+  mutate(cum_prcp = cumsum(prcp)) %>%
+  ungroup() %>% 
+  mutate(new_date = ymd(glue("2022-{month}-{day}"))) %>% 
+  ggplot(aes(x = new_date, y = cum_prcp, group = year, 
+             color = is_this_year, size = is_this_year))+
+  geom_line(show.legend = FALSE) +
+  geom_smooth(aes(group = 1), color = "black", size = 0.3, se = FALSE)+
+  scale_color_manual(breaks = c(F, T),
+                     values = c("lightgray", "dodgerblue"))+
+  scale_size_manual(breaks = c(F, T),
+                     values = c(0.3, 1))+
+  scale_x_date(date_labels = "%B", date_breaks = "2 months") +
+  scale_y_continuous(breaks = seq(0,1000, 100),
+                     labels = seq(0,100, 10),
+                     expand = c(0,0))+
+  labs(x=NULL,
+       y = "Cumulative precipitation (cm)",
+       title = glue("Through {this_month} {this_day}, the cumulative precipitation near Longyearbean, Svalbard is <span style = 'color: dodgerblue'> above average </span> for {this_year}"))+
+  theme(
+    plot.title.position = "plot",
+    plot.title = element_textbox_simple(margin = margin(b=10), size = 16),
+    panel.background = element_blank(),
+    panel.grid = element_blank(),
+    axis.line = element_line()
+  )
+
+ggsave("figures/cumulative_prcp.png",width = 6, height = 5 )
+ 
--- scripts/local_weather.R
+++ scripts/local_weather.R
@@ -12,10 +12,11 @@ inventory_url <- "https://www.ncei.noaa.gov/pub/data/ghcn/daily/ghcnd-inventory.
 inventory <- read_table(inventory_url,
                         col_names = c("station","lat", "lon","variable","start","end"))
 
+
 #local weather station####
 #Second step is to select the area where you want to find the weather stations
-my_lat <- 14.595875057696952 *2 *pi/360 # you need to convert the coordinates                                            in radians for it to make sense
-my_lon <- -90.58743121166637 *2 *pi/360
+my_lat <- 78.21968139431256 *2 *pi/360 # you need to convert the coordinates                                            in radians for it to make sense
+my_lon <- 15.63912339134378 *2 *pi/360
 
 #Third step is to search in the inventory the closest weather station to your position
 my_station <- inventory %>%
@@ -23,7 +24,7 @@ my_station <- inventory %>%
          lon_r = lon *2 *pi/360,
          d = 1.609344 * 3936 * acos((sin(lat_r) * sin(my_lat)) + cos(lat_r) * cos(my_lat) * cos(my_lon - lon_r))
   )%>%
-  filter(start < 1970 & end > 2020) %>%
+  filter(start < 1950 & end > 2021) %>%
   top_n(n = -1, d) %>%
   distinct(station)%>%
   pull(station) 
@@ -49,7 +50,7 @@ local_weather <- read_csv(station_daily,
          TAVG = TAVG/ 10,
          PRCP = PRCP /10) %>%
   rename_all(tolower) %>%
-  mutate(prcp = if_else(prcp < 150, prcp, NA_real_))%>%
+  mutate(prcp = if_else(prcp < 60, prcp, NA_real_))%>%
   drop_na(prcp)
 
 
